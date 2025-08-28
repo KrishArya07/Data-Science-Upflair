96af88e7c677f3b29435b67c603425b8a139b9ec
KrishArya07 <krisharya.official@gmail.com>
2025-08-28 11:33:54 +0530

    python

--- Python/str.py
+++ Python/str.py
@@ -19,10 +19,10 @@
 # print(name.isalpha())  # checks if the string contains only alphabetic characters
 # print(name.isalnum())  # checks if the string contains only alphanumeric characters
 
-#print(name.encode("utf-8"))  # encodes the string to bytes using utf-8 encoding
+# print(name.encode("utf-8"))  # encodes the string to bytes using utf-8 encoding
 # print(name.removeprefix("Upflair"))  # removes the specified prefix from the string
 # print(name.removesuffix("pvt1"))  # removes the specified suffix from the string
-
+# name = "Upflairpvt"
 # print(name.zfill(20))  # pads the string with zeros on the left to make it 20 characters long
 # print(name.center(30, "*"))  # centers the string in a field of 30 characters, padding with '*'
 # print(name.ljust(30, "*"))  # left-justifies the string in a field of 30 characters, padding with '*'
@@ -38,7 +38,7 @@
 # print(name[-1])  # prints the second character
 # print(name[0:7])  # prints characters from index 0 to 7 (7 is excluded)
 
-#formatted_string --> f-strings
+# formatted_string --> f-strings
 # age = 10
 # #text = "my age is" + age
 # #print(text)  # This will raise an error because age is an integer
